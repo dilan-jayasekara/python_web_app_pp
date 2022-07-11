7ede68964cd220fec12ecc465c1a5bb989329278
dkalpa <dilan.kalpa@pitcher.com.au>
2022-07-11 13:43:30 +1000

    Change return text during Lunch and Learn

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, this is a sample Python Web App running on Flask Framework!"
+    return "Hello, this is a sample Python Web App running on Flask Framework! And I just did some modifications to the code and lets see how good DevOps is!"
 
