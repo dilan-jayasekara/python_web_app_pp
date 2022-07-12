80b043aa8b722a6ecad1549df9976f8d4ec5507a
Dilan Jayasekara <dilan.kalpa@pitcher.com.au>
2022-07-12 11:54:01 +1000

    Changes to the app.py file

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, this is a sample Python Web App running on Flask Framework! And I just did some modifications to the code and lets see how good DevOps is!"
+    return "Hello, this is a sample Python Web App running on Flask Framework!!"
 
